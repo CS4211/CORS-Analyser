f4ab4087ad8b8080999fc13e7379dde711158427
bubsnow <bubsnow@gmail.com>
2015-10-18 09:33:25 +0800

    Part 2: Modules and Timeslots Create modules that set properties: module code, time slots and vacancy

---
+++ CORS Analyser/CORS Analyser/Module.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Setting details for each module
+ * Only allowing max 10 time slots
+ * 
+ */
+
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace CORS_Analyser
+{
+    class Module
+    {
+
+        //Properties
+        public String ModuleCode { get; set; }
+        public int vacancy { get; set; }
+        public ArrayList lessonTime = new ArrayList();
+        public int numTS = 0;
+
+        public void addTimeSlot(TimeSlot s)
+        {
+            lessonTime.Add(s);
+        }
+
+        public void removeTimeSlot(TimeSlot s)
+        {
+            lessonTime.Remove(s);
+        }
+        
+
+    }
+
+
+}
---
+++ CORS Analyser/CORS Analyser/TimeSlot.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Store time in 24hour format xxxx
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CORS_Analyser
+{
+    class TimeSlot
+    {
+        //Properties
+
+        public int startTime { get; set; }
+        public int endTime { get; set; }
+
+        public Boolean timeClash(TimeSlot a, TimeSlot b)
+        {
+            if (b.startTime < a.startTime)
+                timeClash(b, a);
+
+            return (a.endTime > b.startTime);
+        }
+    }
+}
